a441e31484e23985fbb991b3cc68c4f01a772b8f
Brian G <brian.gregorius@gmail.com>
2019-06-15 19:17:18 -0400

    refactored code and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
 import { getExchangeRates } from "./components/api";
 import "./App.css";
+import ExchangeGrid from "./components/exchangeGrid";
 
 class App extends Component {
   state = {
     baseCurrency: "USD",
-    currencies: {}
+    baseAmount: (1.0).toFixed(4),
+    rates: {},
+    error: false
   };
 
   componentDidMount() {
-    this.fetchCurriencies(this.state.currencies);
+    this.fetchCurriencies(this.state.rates);
   }
 
   fetchCurriencies = currencies => {
-    getExchangeRates(this.state.baseCurrency, currencies);
+    getExchangeRates(this.state.baseCurrency, currencies)
+      .then(response => {
+        this.setState({
+          rates: Object.assign(this.state.rates, response.rates)
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: `There was an error fetching the currency`
+        });
+      });
   };
 
   render() {
-    return <div className="App">heyo</div>;
+    const { rates, baseCurrency, baseAmount } = this.state;
+    console.log(this.state.rates);
+    return (
+      <div className="App">
+        heyo
+        <ExchangeGrid
+          rates={rates}
+          baseCurrency={baseCurrency}
+          baseAmount={baseAmount}
+        />
+      </div>
+    );
   }
 }
 
